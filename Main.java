98cbc3a59cfe06b2b0db94d0fd10bed14fa705a0
Ondřej Bílek <Bilcuus@gmail.com>
2015-09-24 09:48:19 +0200

    Možnost zatím synchroně odesílat pomocí MQTT protokolu

--- Main.java
+++ Main.java
@@ -10,9 +10,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -28,10 +34,12 @@ public class Main extends AppCompatActivity implements SensorEventListener {
 
     private TextView vibX, vibY, vibZ, accX, accY, accZ, graX, graY, graZ;
     private ToggleButton tgl;
-    private CheckBox save;
+    private RadioButton save, mqtt;
 
     private OutputStreamWriter ostream;
     private FileOutputStream fostream;
+
+    private MqttClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,8 +73,11 @@ public class Main extends AppCompatActivity implements SensorEventListener {
             accX.setText(Float.toString(sensorEvent.values[0]));
             accY.setText(Float.toString(sensorEvent.values[1]));
             accZ.setText(Float.toString(sensorEvent.values[2]));
-            if (save.isChecked())
-                writeLine(sensorEvent);
+            if (save.isChecked()) {
+                writeFile(sensorEvent);
+            } else if (mqtt.isChecked()) {
+                writeStream(sensorEvent);
+            }
         }
     }
 
@@ -80,7 +91,67 @@ public class Main extends AppCompatActivity implements SensorEventListener {
         prepareSensors();
     }
 
-    public void writeLine(SensorEvent sensor){
+    public void writeStream(SensorEvent sensor) {
+        String x = Float.toString(sensor.values[0]);
+        String y = Float.toString(sensor.values[1]);
+        String z = Float.toString(sensor.values[2]);
+
+        String topic = "Viblog";
+        String content = (x + ";" + y + ";" + z + System.getProperty("line.separator"));
+        int qos = 2;
+
+        System.out.println("Publishing message: "+content);
+        MqttMessage message = new MqttMessage(content.getBytes());
+        message.setQos(qos);
+
+        try {
+            client.publish(topic, message);
+            System.out.println("Message published");
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+
+    public void prepareStream() {
+        String broker       = "tcp://iot.eclipse.org:1883";
+        String clientId     = "Android#1";
+        MemoryPersistence persistence = new MemoryPersistence();
+
+        try {
+            client = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            client.connect(connOpts);
+            System.out.println("Connected");
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+
+    public void closeStream() {
+        try {
+            client.disconnect();
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+
+    public void writeFile(SensorEvent sensor){
         String x = Float.toString(sensor.values[0]);
         String y = Float.toString(sensor.values[1]);
         String z = Float.toString(sensor.values[2]);
@@ -132,26 +203,35 @@ public class Main extends AppCompatActivity implements SensorEventListener {
         accZ = (TextView)findViewById(R.id.accZ);
 
         tgl = (ToggleButton)findViewById(R.id.start);
-        save = (CheckBox)findViewById(R.id.log);
+        save = (RadioButton)findViewById(R.id.log);
+        mqtt = (RadioButton)findViewById(R.id.mqtt);
     }
 
     public void tglClick(View v){
         if(tgl.isChecked()) {
-            if (save.isChecked())
+            if (save.isChecked()) {
                 prepareFile();
+            } else if (mqtt.isChecked()) {
+                prepareStream();
+            }
 
             mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
             mSensorManager.registerListener(this, mGravity, SensorManager.SENSOR_DELAY_FASTEST);
             mSensorManager.registerListener(this, mAcceleration, SensorManager.SENSOR_DELAY_FASTEST);
             save.setEnabled(false);
+            mqtt.setEnabled(false);
         } else {
-            if (save.isChecked())
+            if (save.isChecked()){
                 closeFile();
+            } else if(mqtt.isChecked()) {
+                closeStream();
+            }
 
             mSensorManager.unregisterListener(this, mAccelerometer);
             mSensorManager.unregisterListener(this, mGravity);
             mSensorManager.unregisterListener(this, mAcceleration);
             save.setEnabled(true);
+            mqtt.setEnabled(true);
         }
     }
 }
